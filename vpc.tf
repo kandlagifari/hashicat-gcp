4a2f487494992d7351c08fe9edab7d6e048010ff
Kandla Gifari Akbar <kandlagifari@gmail.com>
2023-09-04 09:33:22 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+module "network" {
+  source  = "app.terraform.io/kandla-gifari-msi/network/google"
+  version = "3.4.0"
+  
+  # insert required variables here
+  network_name = "kandla-subnet"
+  project_id = var.project
+  subnets = [
+  {
+    subnet_name   = "kandla-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+}
